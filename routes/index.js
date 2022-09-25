2787c8e225d5443e23e509e427c36ba0337a370d
James Hudson <jmh357@comcast.net>
2022-09-25 16:27:20 -0500

    set up partials

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = Router()
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'this is title - in index.js under routes - UPDATE' })
 })
 
 export { 
