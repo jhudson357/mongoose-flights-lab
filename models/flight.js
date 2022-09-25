4918c57ab1ec65b17f547153b2b0ec40ec0b9c39
James Hudson <jmh357@comcast.net>
2022-09-25 16:13:14 -0500

    create Flight model

---
+++ models/flight.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+// shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema
+
+const flightSchema = new Schema({
+  airline: String,
+  airport: String,
+  flightNo: Number,
+  departs: Date,
+}, {
+  timestamps: true
+})
+
+// compile the schema into a model
+const Flight = mongoose.model('Flight', flightSchema)
+
+// export the model
+export {
+  Flight
+}
