382271bec8e542321df261e44f8af7d6e5f1c483
James Hudson <jmh357@comcast.net>
2022-09-25 19:12:15 -0500

    fix redirect path for create controller function

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,6 +1,21 @@
 // import the flight model
 import { Flight } from "../models/flight.js"
 
+function index(req, res) {
+  console.log('INDEX IS WORKING')
+  Flight.find({})
+  .then(flights => {
+    res.render('flights/index', {
+      flights: flights,
+      title: 'All Flights',
+    })
+  })
+  .catch(error => {
+    console.log(error, 'index error')
+    res.redirect('/flights/index')
+  })
+}
+
 function newFlight(req, res) {
   console.log('this is the new controller working!')
   res.render('flights/new', {
@@ -12,26 +27,11 @@ function create(req, res) {
   Flight.create(req.body)
   .then(flight => {
     console.log(flight)
-    res.redirect('/flights/index')
+    res.redirect('/flights')
   })
   .catch(error => {
-    console.log(error)
+    console.log(error, 'create error')
     res.redirect('/flights/new')
-  })
-}
-
-function index(req, res) {
-  console.log('INDEX IS WORKING')
-  Flight.find({})
-  .then(flights => {
-    res.render('flights/index', {
-      flights: flights,
-      title: 'All Flights',
-    })
-  })
-  .catch(error => {
-    console.log(error)
-    res.redirect('/flights/index')
   })
 }
 
